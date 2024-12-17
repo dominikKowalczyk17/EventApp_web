fca7b785f330a6654f4ed24c6e2a39a89dd52e6b
dkowalczyk <kowalczyk.dominik17@gmail.com>
2024-12-17 14:29:03 +0100

    replace require with import in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import flowbite from "flowbite/plugin";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}',
@@ -51,6 +53,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    flowbite
   ],
 }
